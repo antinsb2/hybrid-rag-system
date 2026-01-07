42df176a4d8e6d29498a26a4453104694eb7516a
antinsb2 <antinsb2@gmail.com>
2026-01-06 22:16:32 -0800

    fix: export HybridRetriever in retrieval module
    - Add HybridRetriever to module exports
    - Fix import error in benchmarks

--- src/rag/retrieval/__init__.py
+++ src/rag/retrieval/__init__.py
@@ -5,6 +5,19 @@ Retrieval components for RAG.
 from .types import RetrievalResult
 from .query_processor import QueryProcessor
 from .dense_retriever import DenseRetriever
+from .sparse_retriever import SparseRetriever
 from .filters import ResultFilter, ResultRanker
+from .hybrid_retriever import HybridRetriever
 
-__all__ = ['RetrievalResult', 'QueryProcessor', 'DenseRetriever', 'ResultFilter', 'ResultRanker']
+__all__ = [
+    'RetrievalResult',
+    'QueryProcessor',
+    'DenseRetriever',
+    'SparseRetriever',
+    'HybridRetriever',
+    'ResultFilter',
+    'ResultRanker',
+    'ReciprocalRankFusion',
+    'WeightedFusion',
+    'SimpleCombination'
+]
